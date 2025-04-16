c2a12bd25a9694dad1b4d7188c95cfda0d844fed
Itx-Ahmed <ahmadiqbal858687@gmail.com>
2025-04-16 05:39:13 -0700

    about-and-connect-section designed

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,9 +1,11 @@
+import AboutAndConnectSection from "../../components/about/about-and-connect-section";
 import Innofusiontech from "../../components/about/Innofusiontech";
 
 function About() {
   return (
     <>
       <Innofusiontech />
+      <AboutAndConnectSection />
     </>
   );
 }
